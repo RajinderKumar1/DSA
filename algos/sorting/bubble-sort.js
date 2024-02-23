03e0e5a493bc2301aaf448c7bea1aa30f8ad90a8
RajinderKumar1 <idrajinder9@gmail.com>
2024-02-23 22:32:54 +0530

    bubble sort

---
+++ algos/sorting/bubble-sort.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i; j++) {
+            if (arr[j] < arr[j - 1]) {
+                // swap
+                let temp = arr[j];
+                arr[j] = arr[j-1]
+                arr[j-1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+let arr = [7, 4, 6, 9, 3];
+
+console.log(bubbleSort(arr));
